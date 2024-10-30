cd2fcf77953a11647421dbb71827bbc6f4a36565
Nihar <niharnayak331@gmail.com>
2024-10-30 21:33:29 +0530

    array done

---
+++ src/nihar_automation/Oops.java
@@ -0,0 +1,40 @@
+package nihar_automation;
+
+public class Oops {
+
+        private String Car_Name;
+        private String Car_Colour;
+        private  int Car_wheels;
+        private int Car_sheets;
+        private float Top_speed;
+        //Constructor
+    public void car(){
+
+        }
+
+        public  void car_Started(){
+            System.out.println("Car has been started..... ");
+
+        }
+        public  void car_InDrivingMode(){
+            System.out.println("Car is running.....");
+
+        }
+        public void car_Stopped(){
+            System.out.println("Car has Stoped.....");
+
+        }
+
+        public static void main(String[] args) {
+            Oops obj =new Oops();
+            obj.car_InDrivingMode();
+            obj.car();
+            obj.car_Started();
+            obj.car_Stopped();
+
+        }
+
+
+
+    }
+
