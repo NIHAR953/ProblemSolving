9df93a70748e5c717271021f6b87450c7a38c2e1
Nihar <niharnayak331@gmail.com>
2024-11-13 06:50:47 +0530

    ArrayList

---
+++ src/nihar_automation/CollectionFrameWork/ArrayListDemo.java
@@ -0,0 +1,8 @@
+package nihar_automation.CollectionFrameWork;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList mylist=new ArrayList();
+        
+    }
+}
